5eaf2f343a48f8916fc0e0efb3386f49d6bb0abb
SyStem-5 <g1-assassin@hotmail.com>
2019-03-17 00:07:48 +0100

    Generating secure credentials for mqtt accounts. Adding/Removing mqtt users from the database.

--- update_manager/mosquitto/main.py
+++ update_manager/mosquitto/main.py
@@ -1,21 +1,48 @@
 # Disable annoying 'no member' error
 # pylint: disable=E1101
 
+import base64
+import hashlib
+import os
+
 from update_manager.models import MQTTACL, MQTTUsers
+
+pbkdf2_iterations = 100000
+digest_alg = 'sha256'
 
 
 def add_user(username, password, isSuperuser=0):
+
+    if not isinstance(isSuperuser, int):
+        return
+
+    salt = base64.b64encode(os.urandom(20))
+
+    pbkdf2_hash = hashlib.pbkdf2_hmac(
+        digest_alg,
+        str.encode(password),
+        salt,
+        pbkdf2_iterations
+    )
+
+    complete_hash = 'PBKDF2${}${}${}${}'.format(
+        digest_alg,
+        pbkdf2_iterations,
+        salt.decode("utf-8"),
+        base64.b64encode(pbkdf2_hash).decode("utf-8")
+    )
+
+    clean_username = ''.join(e for e in username.lower() if e.isalnum())
+
     MQTTUsers.objects.create(
-        username=username,
-        password=password,
+        username=clean_username,
+        password=complete_hash,
         superuser=isSuperuser
     ).save()
 
-    #add_acl(self.username, 'nowhere', 7)
-
 
 def remove_user(username):
-    pass
+    MQTTUsers.objects.filter(username=username).delete()
 
 
 def add_acl(username, topic, permissions=5):
