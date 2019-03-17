11cd7a2a8df71216a937248f1e0fafead8d722b1
SyStem-5 <g1-assassin@hotmail.com>
2019-03-17 01:20:17 +0100

    Custom method decorator for mqtt authentication.

---
+++ update_manager/decorators.py
@@ -0,0 +1,44 @@
+# Disable annoying 'no member' error
+# pylint: disable=E1101
+
+import base64
+from functools import wraps
+
+from django.http import JsonResponse
+
+from update_manager.models import MQTTUsers
+from update_manager.mosquitto.main import generate_hash
+
+
+def mqtt_auth(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        req = request.GET
+
+        received_fields = list(req)
+        required_fields = ['username', 'password']
+
+        for field in required_fields:
+            if field not in received_fields:
+                return JsonResponse({'message': 'Error: missing field/s.'})
+
+        clean_username = ''.join(e for e in req['username'].lower() if e.isalnum())
+
+        try:
+            user = MQTTUsers.objects.get(username=clean_username)
+        except:
+            return JsonResponse({'message': 'Error: Unknown user.'})
+
+        proposed_user_salt = base64.b64decode(
+            user.password.split('$')[3].encode('utf-8'))
+
+        if generate_hash(req['password'], proposed_user_salt) != user.password:
+            return JsonResponse({'message': 'Error: Unknown user.'})
+
+        # print('-'*5+'MQTT'+'-'*5)
+        # print('User authenticated.')
+        # print('-'*14)
+
+        return function(request, *args, **kwargs)
+
+    return wrap
